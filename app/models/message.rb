d8de574262f7cda03ed74b89f191a38e92c9f637
ando <ando@nurve.jp>
2016-02-04 17:10:23 +0900

    relese Test

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
-	validates :title, :presence => true, :length => { :maximum => 10}
-	validates :body, :presence => true, :length => { :maximum => 100}
+  validates :title, :presence => true, :length => { :maximum => 10 }
+  validates :body, :presence => true, :length => { :maximum => 100 }
 
-	def self.message_list
-		return Message.all.order(:created_at => :desc)
-	end
+  def self.message_list
+    return Message.all.order(:created_at => :desc)
+  end
 end

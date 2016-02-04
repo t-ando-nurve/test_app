d8e2c84cc80e654ba9f0d5db0b78be1039b88e83
ando <ando@nurve.jp>
2016-02-04 14:05:22 +0900

    initial release3

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+
+	def index
+		@messages = Message.message_list
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new(params.require(:message).permit(:title,:body))
+		if @message.save
+			redirect_to :action => :index
+		else
+			@messages = Message.message_list
+			render :index
+		end
+	end
+end

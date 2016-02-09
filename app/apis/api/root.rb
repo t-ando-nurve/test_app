505820d5d355a43b02496cf74dad26d744f0294c
ando <ando@nurve.jp>
2016-02-09 21:13:12 +0900

    Swagger commit 2
    Swagger が動くようになりました。

--- app/apis/api/root.rb
+++ app/apis/api/root.rb
@@ -6,5 +6,10 @@ module API
     mount API::Ver1::Root
     #mount API::Ver2::Root
 
+    add_swagger_documentation(
+      api_version: "v1",
+      base_path: "/",
+      hide_documentation_path: true
+    )
   end
 end
--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url      = "swagger_doc.json"
 GrapeSwaggerRails.options.app_name = 'GrapeSwagger'
-GrapeSwaggerRails.options.app_url  = '/'
+GrapeSwaggerRails.options.app_url  = '/api/'
